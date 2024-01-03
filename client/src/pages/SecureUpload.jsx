ba860e03ba873b1811e47086cc5d5f194929c2d0
Psng Abu <abupsngdscz@gmail.com>
2024-01-03 22:51:30 +0530

    initiated Home secureupload and upload pags

---
+++ client/src/pages/SecureUpload.jsx
@@ -0,0 +1,9 @@
+const SecureUpload = () => {
+    return (
+        <div>
+            <h1>SecureUpload pages</h1>
+        </div>
+    )
+}
+
+export default SecureUpload
---
+++ client/src/pages/Upload.jsx
@@ -0,0 +1,11 @@
+
+
+const Upload = () => {
+    return (
+        <div>
+            <h1>Upload images and videos</h1>
+        </div>
+    )
+}
+
+export default Upload
