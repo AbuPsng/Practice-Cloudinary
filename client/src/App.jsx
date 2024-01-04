ea2eca3ad71661878497f72b90f8a759c94971d0
Psng Abu <abupsngdscz@gmail.com>
2024-01-04 08:01:33 +0530

    install mongoose express multer and dotenv

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,12 @@
+import { Link, Outlet } from "react-router-dom"
 
 function App() {
 
   return (
     <>
+      <h3>Upload files using Multer and Cloudinary Services in MERN stack project</h3>
+      <Link to="/">home </Link>|<Link to="/upload"> Upload </Link>|<Link to="secure_upload"> Secure Upload</Link>
+      <Outlet />
     </>
   )
 }
--- client/src/pages/SecureUpload.jsx
+++
@@ -1,9 +0,0 @@
-const SecureUpload = () => {
-    return (
-        <div>
-            <h1>SecureUpload pages</h1>
-        </div>
-    )
-}
-
-export default SecureUpload
